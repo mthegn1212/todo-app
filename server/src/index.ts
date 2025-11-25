3a72daf2878e15d7d01b1f138ddf36f73c03a2c2
mthegn1212 <minhthangcoder@gmail.com>
2025-11-26 00:03:34 +0700

    chore: Update link Backend in index.ts (server)

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,11 +10,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-// app.use(cors({
-//   origin: "http://localhost:5173",
-//   credentials: true 
-// }));
+// app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://my-todo-api-ril3.onrender.com"
+  ],
+  credentials: true 
+}));
+
 app.use(express.json());
 
 // Routes
