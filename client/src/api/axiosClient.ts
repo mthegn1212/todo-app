577bc28ba590e5a6bd9d39630d6621efffc8c78c
mthegn1212 <minhthangcoder@gmail.com>
2025-11-25 23:53:03 +0700

    chore: Update link Backend deploy.

--- client/src/api/axiosClient.ts
+++ client/src/api/axiosClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://my-todo-api-ril3.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
