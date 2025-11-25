ab72d45944eb9f3bd9a6a6f8b712e81a30a737d8
mthegn1212 <minhthangcoder@gmail.com>
2025-11-26 00:11:46 +0700

    fix: linkd deploy Backend in client.

--- client/src/api/axiosClient.ts
+++ client/src/api/axiosClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://my-todo-api-ril3.onrender.com",
+  baseURL: "https://my-todo-api-ril3.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
