2c671d01552b2a0eb725489472f6fe918e79f834
mthegn1212 <minhthangcoder@gmail.com>
2025-11-26 21:42:18 +0700

    fix: add vercel domain to cors whitelist

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,9 @@ app.use(cors({
     "http://localhost:5173",
     "https://my-todo-api-ril3.onrender.com"
   ],
-  credentials: true 
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 app.use(express.json());
